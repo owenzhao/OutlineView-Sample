440dbed0e833577fd897426da513feab86268815
肇鑫（Zhao Xin） <owenzx@gmail.com>
2021-03-20 20:05:38 +0800

    Initial Commit

---
+++ OutlineView Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OutlineView Sample
+//
+//  Created by zhaoxin on 2021/3/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
